fb36d2d23c9bb7e2b19dd41a2be0a6dd02fd5709
hellcy <chengyuan82281681@hotmail.com>
2021-11-29 22:41:59 +1100

    Spring Boot CommandLineRunner with bootstrap data

--- src/main/java/guru/springfamework/repositories/CategoryRepository.java
+++ src/main/java/guru/springfamework/repositories/CategoryRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by jt on 9/24/17.
  */
+// Category repository extends JPA repository (h2 in memory database)
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
